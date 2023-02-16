c44f7efe526e32561437de1d19a55ea231c9cfb7
Ying Cai <ying.cai@analog.com>
2023-02-15 20:53:58 -0600

    fiinsh local full per test

--- debug_script/per_test/local_full_per_test.sh
+++ debug_script/per_test/local_full_per_test.sh
@@ -2,31 +2,34 @@
 
 echo
 echo "#################################################################################################################"
-echo "# ./local_full_per_test.sh 1_MSDK 2_DEV_TOOLS 3_PER_TOOL 4_CHIP_UC                                              #"
+echo "# ./local_full_per_test.sh 1_MSDK 2_CHIP_UC 3_BRD_TYPE                                                          #"
 echo "# Example:                                                                                                      #"
 echo "#     ./local_full_per_test.sh            \                                                                     #"
 echo "#         ~/Workspace/temp/msdk-me18      \                                                                     #"
-echo "#         ~/Workspace/yc/msdk_dev_tools   \                                                                     #"
-echo "#         ~/Workspace/yc/msdk_ci_per_tool \                                                                     #"
-echo "#         MAX32655 2>&1 | tee test.log                                                                          #"
+echo "#         MAX32690 WLP_V1 2>&1 | tee test.log                                                                   #"
 echo "#################################################################################################################"
 echo
 echo $0 $@
 echo
 
+if [[ $# -ne 3 ]]; then
+    echo "Invalid argument count."
+    exit 1
+fi
+
+# Example:
+# ./local_full_per_test.sh ~/Workspace/yc/msdk_open MAX32690 WLP_V1 2>&1 | tee test.log
+
 #----------------------------------------------------------------------------------------------------------------------
 # Validate the input arguments
 TEST_MSDK=$1
-MSDK_DEV_TOOLS=$2
-PER_TOOL=$3
-CHIP_UC=$4
-
+CHIP_UC=$2
+BRD_TYPE=$3
 #----------------------------------------------------------------------------------------------------------------------
 # Prepare the test repositories
 echo "     TEST_MSDK: ${TEST_MSDK}"
-echo "MSDK_DEV_TOOLS: ${MSDK_DEV_TOOLS}"
-echo "      PER_TOOL: ${PER_TOOL}"
 echo "       CHIP_UC: ${CHIP_UC}"
+echo "      BRD_TYPE: ${BRD_TYPE}"
 echo ""
 
 TEST_ROOT=`realpath ~/temp_safe_to_del`
@@ -35,11 +38,10 @@ rm -rf ${TEST_ROOT}
 mkdir -p ${TEST_ROOT}
 
 cp -Rp ${TEST_MSDK}      ${TEST_ROOT}/msdk/
-cp -Rp ${MSDK_DEV_TOOLS} ${TEST_ROOT}/msdk/
-cp -Rp ${PER_TOOL}       ${TEST_ROOT}/msdk/
 
 MSDK=${TEST_ROOT}/msdk
 
+# This will be used to search the test used in the configuration json file.
 TEST=local_full_per_test
 
 cd ${TEST_ROOT}
@@ -49,6 +51,7 @@ echo ""
 echo "Show the PWD folder."
 tree -a -L 2
 echo ""
+
 #----------------------------------------------------------------------------------------------------------------------
 # Get the configuration
 CONFIG_FILE=/home/$USER/Workspace/ci_config/RF-PHY-closed.json
@@ -72,7 +75,8 @@ echo DO_MAX32655: ${DO_MAX32655}
 echo DO_MAX32665: ${DO_MAX32665}
 echo DO_MAX32690: ${DO_MAX32690}
 echo DO_MAX32690_WLP: ${DO_MAX32690_WLP}
-echo
+echo ""
+
 MAX32655_PKG_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32655_pkglen_range'])")
 MAX32665_PKG_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32665_pkglen_range'])")
 MAX32690_PKG_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32690_pkglen_range'])")
@@ -85,7 +89,8 @@ echo MAX32655_PKG_RA: ${MAX32655_PKG_RA}
 echo MAX32665_PKG_RA: ${MAX32665_PKG_RA}
 echo MAX32690_PKG_RA: ${MAX32690_PKG_RA}
 echo MAX32690_WLP_PKG_RA: ${MAX32690_WLP_PKG_RA}
-echo
+echo ""
+
 export MAX32655_PHY_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32655_phy_range'])")
 export MAX32665_PHY_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32665_phy_range'])")
 export MAX32690_PHY_RA=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32690_phy_range'])")
@@ -98,7 +103,8 @@ echo MAX32655_PHY_RA: ${MAX32655_PHY_RA}
 echo MAX32665_PHY_RA: ${MAX32665_PHY_RA}
 echo MAX32690_PHY_RA: ${MAX32690_PHY_RA}
 echo MAX32690_WLP_PHY_RA: ${MAX32690_WLP_PHY_RA}
-echo
+echo ""
+
 MAX32655_STEP=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32655_step'])")
 MAX32665_STEP=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32665_step'])")
 MAX32690_STEP=$(python3     -c "import json; import os; obj=json.load(open('${CONFIG_FILE}')); print(obj['tests']['${TEST}']['max32690_step'])")
@@ -111,13 +117,13 @@ echo MAX32655_STEP: ${MAX32655_STEP}
 echo MAX32665_STEP: ${MAX32665_STEP}
 echo MAX32690_STEP: ${MAX32690_STEP}
 echo MAX32690_WLP_STEP: ${MAX32690_WLP_STEP}
-echo
+echo ""
 
 #----------------------------------------------------------------------------------------------------------------------
 # Prepare the arguments for script.                    
 #MSDK=TODO
 #CHIP_UC=TODO
-$MSDK/msdk_dev_tools/scripts/msdk_per_skip_check.sh \
+$MSDK/.github/workflows/scripts/msdk_per_skip_check.sh \
     $NO_SKIP \
     $MSDK    \
     $CHIP_UC
@@ -127,26 +133,35 @@ if [[ $? -ne 0 ]]; then
 fi
 #------------------------------------------------
 # Prepare the arguments for function call
-NEW_NAME=DO_${CHIP_UC}
-DO_IT=${!NEW_NAME}
-echo DO_IT: ${DO_IT}
-if [ "${DO_IT}" == "0" ]; then
-    echo "Skip the test for MAX32655."
-    exit 0
-fi
-
-BRD2_CHIP_LC=${CHIP_UC}
 BRD1=nRF52840_1
+BRD2_TYPE=EvKit_V1EvKit_V1EvKit_V1v
+NEW_NAME=DO_${CHIP_UC}
 
-if [ "${CHIP_UC}" == "MAX32655" ]; then
+if [ "${CHIP_UC}" == "MAX32690" ] && [ "${BRD_TYPE}" == "WLP_V1" ]; then
+    BRD2=max32690_board_A1
+    BRD2_TYPE=WLP_V1
+    NEW_NAME=DO_${CHIP_UC}_WLP
+elif [ "${CHIP_UC}" == "MAX32655" ]; then
     BRD2=max32655_board_2
 elif [ "${CHIP_UC}" == "MAX32665" ]; then
     BRD2=max32665_board_w3
 elif [ "${CHIP_UC}" == "MAX32690" ]; then
     BRD2=max32690_board_w2
+else
+    echo "Invalid chip and board type."
+    exit 2
 fi
+
 echo BRD2: ${BRD2}
+BRD2_CHIP_LC=${CHIP_UC,,}
 echo ""
+
+DO_IT=${!NEW_NAME}
+echo DO_IT: ${DO_IT}
+if [ "${DO_IT}" == "0" ]; then
+    echo "Skip the test for ${CHIP_UP} ${BRD_TYPE}."
+    exit 0
+fi
 
 CURR_TIME=$(date +%Y-%m-%d_%H-%M-%S)
 CURR_JOB_FILE=/home/$USER/Workspace/Resource_Share/History/msdk_simple_per_test_${CURR_TIME}_${BRD2_CHIP_LC}.txt
@@ -155,11 +170,11 @@ CURR_LOG=/home/$USER/Workspace/Resource_Share/Logs/msdk_simple_per_test_${CURR_T
 echo "::set-env name=CURR_LOG::${CURR_LOG}"
 RESULT_PATH=~/Workspace/ci_results/per
 res=${RESULT_PATH}/msdk-${CURR_TIME}
-all_in_one=${res}_${BRD2_CHIP_LC}.csv
+all_in_one=${res}_${BRD2_CHIP_LC}_${BRD2_TYPE}.csv
 echo "::set-env name=all_in_one::${all_in_one}"
 
 #------------------------------------------------
-${MSDK}/msdk_dev_tools/scripts/board_per_test.sh \
+${MSDK}/.github/workflows/scripts/board_per_test.sh \
     $MSDK      \
     $BRD1      \
     $BRD2      \
@@ -179,5 +194,5 @@ fi
 cd ${MSDK}
 echo PWD: `pwd`
 echo
-chmod u+x ${MSDK}/msdk_dev_tools/scripts/unlock_plot.sh
-${MSDK}/msdk_dev_tools/scripts/unlock_plot.sh ${MSDK} ${CURR_JOB_FILE} ${all_in_one} True 2>&1 | tee -a ${CURR_LOG}
+chmod u+x ${MSDK}/.github/workflows/scripts/unlock_plot.sh
+${MSDK}/.github/workflows/scripts/unlock_plot.sh ${MSDK} ${CURR_JOB_FILE} ${all_in_one} True 2>&1 | tee -a ${CURR_LOG}
